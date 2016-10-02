30ce9ab8dbdfc68059ace5c87227d20ebad34d84
Wendy Smoak <wsmoak@gmail.com>
2016-10-02 17:19:00 -0400

    Update mix.exs with target and custom system

--- hello_wifi/mix.exs
+++ hello_wifi/mix.exs
@@ -1,20 +1,21 @@
 defmodule HelloWifi.Mixfile do
   use Mix.Project
 
-  @target System.get_env("NERVES_TARGET") || "rpi3"
+  @target System.get_env("NERVES_TARGET") || "rpi2"
 
   def project do
     [app: :hello_wifi,
      version: "0.0.1",
      elixir: "~> 1.3",
-     archives: [nerves_bootstrap: "~> 0.1.3"],
+     archives: [nerves_bootstrap: "~> 0.2.0"],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      target: @target,
      deps_path: "deps/#{@target}",
      build_path: "_build/#{@target}",
      aliases: aliases(),
-     deps: deps() ++ system(@target)]
+     deps: deps()
+   ]
   end
 
   # Configuration for the OTP application.
@@ -26,13 +27,10 @@ defmodule HelloWifi.Mixfile do
   end
 
   defp deps do
-    [{:nerves, "~> 0.3.0"},
-     {:nerves_interim_wifi, "~> 0.0.2"}]
-  end
-
-  def system(target) do
     [
-     {:"nerves_system_#{target}", "~> 0.6"}
+      {:nerves, "~> 0.3.0"},
+      {:nerves_interim_wifi, "~> 0.0.2"},
+      {:nerves_system_rpi2, git: "https://github.com/wsmoak/nerves_system_rpi2", branch: "with_8188"}
     ]
   end
 
